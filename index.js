3c5eb4411fc406cc4eac33d79ac27d9e57b3f831
lucas rodrigues <rodrigueslucas062@gmail.com>
2022-03-29 16:11:59 -0300

    commit

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const Discord = require ('discord.js');
 
+const bot = new Discord.Client();
 const { Client, Intents } = require('discord.js');
-// Criar uma nova instancia do cliente 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 //token de autenticação
-const token = 'OTU4MDIyNTgyNzU2NTg5NTg4.YkHSDg.OgehvvPQFVnv6KNuuv-yLEEt4Jw';
+//const token = '';
+var config = require('./config.json');
+client.login(config.TOKEN);
 
-bot.login(token)
+bot.login(TOKEN)
 bot.on('ready', () => {
     console.log('hey-hey-hey criançada')
 })
