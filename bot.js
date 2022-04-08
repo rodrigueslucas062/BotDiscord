cc45e878436a6ca2070ded3d5813c74cdb9310e4
lucas rodrigues <rodrigueslucas062@gmail.com>
2022-04-08 01:22:39 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,9 +3,9 @@ const client = new Discord.Client();
 const config = require("./config.json")
 
 client.on("ready", () => {
-     console.log('Bot iniciado com ${client.users.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores');
-     client.user.setGame('estou em ${client.guilds.size} servidores');
-    });
+     console.log("hey-hey-hey criançada!");
+     //client.user.setGame('estou em ${client.guilds.size} servidores');
+});
 
 client.on("guildCreate", guild => {
     console.log('O bot entrou no servidor ${guild.name} (id: {guild.id}, população: ${guild.memberCount}.');
