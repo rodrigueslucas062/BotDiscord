8700283a2512998556f0855f58786ec70c132d2c
lucas rodrigues <rodrigueslucas062@gmail.com>
2022-04-13 23:46:17 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const Discord = require ('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json")
 
+
+//Status do bot
 client.on("ready", () => {
      console.log(`hey-hey-hey criançada! Estamos em ${client.guilds.cache.size} servidores`);
      client.user.setActivity('Meu tempo fora');
@@ -18,6 +20,19 @@ client.on("guildDelete", guild => {
 });
 
 client.on("message", async message =>{
+    if(message.author.bot) return;
+    if(message.channel.type === "dm")  return;
+
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const comando = args.shift().toLowerCase();
+    
+///estrutura para criar comando
+    if (comando === "ping"){
+        const m  = await message.channel.send("Ping?")
+        m.edit(`Ping! A latencia é ${m.createdTimestamp - message.createdTimestamp}ms. A latencia da API é ${Math.round(client.ping)}ms.`)
+    }
+
+    
 
 });
 
